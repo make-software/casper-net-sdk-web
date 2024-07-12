665da9a9f6ca5cb80cc9216573e9d3e14c459bfd
David Hernando <david.hernando@make.services>
2024-07-12 09:42:19 +0200

    show lowercase hex values without checksums align how deploy and transaction details are shown
    Signed-off-by: David Hernando <david.hernando@make.services>

--- Docs/Demos/NCTLWebExplorer/Utils/FormatExtensions.cs
+++ Docs/Demos/NCTLWebExplorer/Utils/FormatExtensions.cs
@@ -1,16 +1,24 @@
-﻿namespace NCTLWebExplorer.Utils;
+﻿using Casper.Network.SDK.Types;
+
+namespace NCTLWebExplorer.Utils;
 
 public static class FormatExtensions
 {
     public static string FormatHash(this string hash)
     {
         var len = hash.Length;
-        return $"{hash.Substring(0, 8)}....{hash.Substring(len-8, 8)}";
+        return $"{hash.Substring(0, 5)}...{hash.Substring(len-5, 5)}".ToLower();
     }
     
     public static string FormatPublicKey(this string pk)
     {
         var len = pk.Length;
-        return $"{pk.Substring(0, 4)}....{pk.Substring(len-4, 4)}";
+        return $"{pk.Substring(0, 5)}...{pk.Substring(len-5, 5)}";
+    }
+    
+    public static string FormatPublicKey(this PublicKey pk)
+    {
+        var len = pk.ToString().Length;
+        return $"{pk.ToString().Substring(0, 5)}...{pk.ToString().Substring(len-5, 5)}";
     }
 }
