e8ab7e01e10790c64b483ec607985120a08933bc
David Hernando <david.hernando@make.services>
2024-11-25 17:25:22 +0100

    added memory cache for items count
    Signed-off-by: David Hernando <david.hernando@make.services>

--- Docs/Demos/NCTLWebExplorer/Program.cs
+++ Docs/Demos/NCTLWebExplorer/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+builder.Services.AddMemoryCache();
+
 // Add Radzen services.
 builder.Services.AddScoped<NotificationService>();
 
